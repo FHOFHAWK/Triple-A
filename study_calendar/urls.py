22f9f80ae86225c151486ff86f03d288d772159d
TrickyF0x <sealekssaa@gmail.com>
2021-08-04 12:46:22 +0300

    accounts actions and authorization added

--- study_calendar/urls.py
+++ study_calendar/urls.py
@@ -12,6 +12,6 @@ router.register(r'groups', views.GroupViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/',  admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
