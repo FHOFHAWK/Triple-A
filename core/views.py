629642d539e9d7580f715fbc5e79f475f2baab41
Romanov Andrey <andrey_romanof@mail.ru>
2021-08-04 12:46:43 +0300

    Начал делать регистрацию

--- core/views.py
+++ core/views.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
+from rest_framework import serializers, viewsets
 from rest_framework import permissions
-from core.serializers import UserSerializer, GroupSerializer
+from core.serializers import UserSerializer, GroupSerializer, RegistrationSerializer
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -20,3 +24,16 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+@api_view(['POST'])
+def registration_view(request):
+    if request.method == 'POST':
+        serializer = RegistrationSerializer(data=request.data)
+        data = {}
+        if serializer.is_valid():
+            person = serializer.save()
+            data['response'] = "succsessfully registered user"
+            data['email'] = person.email
+        else:
+            data = serializer.errors
+        return Response(data)
