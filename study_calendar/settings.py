ab5dab2a63f7e426823b731ae1f44b3c2c76749e
TrickyF0x <sealekssaa@gmail.com>
2021-08-04 13:27:18 +0300

    login url added

--- study_calendar/settings.py
+++ study_calendar/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import  os
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -115,6 +115,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+LOGIN_URL = '/accounts/login'
 
 LOGIN_REDIRECT_URL = '/'
 
